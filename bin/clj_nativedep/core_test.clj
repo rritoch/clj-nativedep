ce3d1131c0a63f6dbf84f740e38a4550318d9062
rritoch <rritoch@gmail.com>
2014-05-23 01:06:30 +0800

    Updated documentation

---
+++ bin/clj_nativedep/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-nativedep.core-test
+  (:require [clojure.test :refer :all]
+            [clj-nativedep.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
